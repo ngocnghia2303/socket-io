18d43ec9e1d3e604222c4c29160d2e96a10f4edd
ngocnghia2303 <ngocty756@gmail.com>
2021-03-14 23:01:44 +0700

    form & button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,10 +4,24 @@ socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+//setup selector elements
+const $messageForm = document.querySelector('#message-form')
+const $formInput = $messageForm.querySelector('Input')
+const $formButton = $messageForm.querySelector('Button')
+const $sendGeo = document.querySelector('#send-geo')
+
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    $formButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message, (error) => {
+        $formButton.removeAttribute('disabled')
+        //setup formInput have empty value after send message
+        $formInput.value = ''
+        $formInput.focus()
+
         if (error) {
             return console.log(error)
         }
@@ -15,10 +29,12 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     })
 })
 
-document.querySelector('#send-geo').addEventListener('click', () => {
+$sendGeo.addEventListener('click', () => {
     if (!navigator.geolocation.getCurrentPosition) {
         console.log('Can not get current position!')
     }
+    
+    $sendGeo.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
         const coords = {
             'long': position.coords.longitude,
@@ -27,6 +43,7 @@ document.querySelector('#send-geo').addEventListener('click', () => {
         // const location = `Toa do cua tao ne Kim Anh => long: ${coords.long}, lat: ${coords.lat}`
         const myaddress = `https://www.google.com/maps?q=${coords.lat},${coords.long}`
         socket.emit('sendLocation', myaddress, () => {
+            $sendGeo.removeAttribute('disabled')
             return console.log('Location shared!')
         })
 
